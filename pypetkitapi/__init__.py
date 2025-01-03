ab5a9b22ef5a1b5fb8f98423c1128fe59926bdcc
Jezza34000 <57314417+Jezza34000@users.noreply.github.com>
2025-01-03 15:05:10 +0100

    :technologist: add reference to __all__

--- pypetkitapi/__init__.py
+++ pypetkitapi/__init__.py
@@ -3,6 +3,7 @@
 from .client import PetKitClient
 from .command import (
     DeviceAction,
+    DeviceCommand,
     FeederCommand,
     LBCommand,
     LitterCommand,
@@ -15,6 +16,7 @@ from .const import (
     D4,
     D4H,
     D4S,
+    D4SH,
     DEVICES_FEEDER,
     DEVICES_LITTER_BOX,
     DEVICES_PURIFIER,
@@ -30,37 +32,55 @@ from .const import (
     W5,
     RecordType,
 )
+from .containers import Pet
+from .exceptions import PetkitAuthenticationError, PypetkitError
+from .feeder_container import Feeder, RecordsItems
+from .litter_container import Litter, LitterRecord, WorkState
 from .medias import MediaHandler, MediasFiles
+from .purifier_container import Purifier
+from .water_fountain_container import WaterFountain
 
 __version__ = "1.7.10"
 
 __all__ = [
-    "MediasFiles",
-    "MediaHandler",
+    "CTW3",
     "D3",
     "D4",
     "D4H",
     "D4S",
+    "D4SH",
+    "DEVICES_FEEDER",
+    "DEVICES_LITTER_BOX",
+    "DEVICES_PURIFIER",
+    "DEVICES_WATER_FOUNTAIN",
+    "DeviceAction",
+    "DeviceCommand",
     "FEEDER",
     "FEEDER_MINI",
+    "Feeder",
+    "FeederCommand",
+    "K2",
+    "K3",
+    "LBCommand",
+    "Litter",
+    "LitterCommand",
+    "LitterRecord",
+    "MediaHandler",
+    "MediasFiles",
+    "Pet",
+    "PetCommand",
+    "PetKitClient",
+    "PetkitAuthenticationError",
+    "PurMode",
+    "Purifier",
+    "PypetkitError",
+    "RecordType",
+    "RecordsItems",
     "T3",
     "T4",
     "T5",
     "T6",
     "W5",
-    "CTW3",
-    "K2",
-    "K3",
-    "DEVICES_FEEDER",
-    "DEVICES_LITTER_BOX",
-    "DEVICES_WATER_FOUNTAIN",
-    "DEVICES_PURIFIER",
-    "RecordType",
-    "PetKitClient",
-    "FeederCommand",
-    "LitterCommand",
-    "PetCommand",
-    "LBCommand",
-    "DeviceAction",
-    "PurMode",
+    "WaterFountain",
+    "WorkState",
 ]
