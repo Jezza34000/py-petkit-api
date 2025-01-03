ad7eb410a00bd6248209c217dd55fc8f6f688757
Jezza34000 <57314417+Jezza34000@users.noreply.github.com>
2025-01-03 16:10:19 +0100

    bump version 1.8.0 -> 1.8.1

--- pypetkitapi/__init__.py
+++ pypetkitapi/__init__.py
@@ -40,7 +40,7 @@ from .medias import MediaHandler, MediasFiles
 from .purifier_container import Purifier
 from .water_fountain_container import WaterFountain
 
-__version__ = "1.8.0"
+__version__ = "1.8.1"
 
 __all__ = [
     "CTW3",
