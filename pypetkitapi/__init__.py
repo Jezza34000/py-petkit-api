e365e97bfa11d047a5c6f4e63b26b31776619ee0
Jezza34000 <57314417+Jezza34000@users.noreply.github.com>
2025-04-08 15:32:04 +0200

    bump version 1.16.0 -> 1.16.1

--- pypetkitapi/__init__.py
+++ pypetkitapi/__init__.py
@@ -51,7 +51,7 @@ from .media import DownloadDecryptMedia, MediaCloud, MediaFile, MediaManager
 from .purifier_container import Purifier
 from .water_fountain_container import WaterFountain
 
-__version__ = "1.16.0"
+__version__ = "1.16.1"
 
 __all__ = [
     "CTW3",
