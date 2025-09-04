88b5e5d17391ca31e2d7a3e21cf7227e123601ab
Jezza34000 <57314417+Jezza34000@users.noreply.github.com>
2025-09-04 14:42:48 +0200

    :white_check_mark: update tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -9,39 +9,41 @@ class TestUtils(unittest.TestCase):
 
     @patch("pypetkitapi.utils.ZoneInfo")
     @patch("pypetkitapi.utils.datetime")
-    def test_get_timezone_offset_valid(self, mock_datetime, mock_zoneinfo):
+    async def test_get_timezone_offset_valid(self, mock_datetime, mock_zoneinfo):
         # Mock datetime and ZoneInfo
         mock_zoneinfo.return_value = "MockedZone"
         mock_now = mock_datetime.now.return_value
         mock_now.utcoffset.return_value.total_seconds.return_value = 3600
 
-        offset = get_timezone_offset("MockedZone")
+        offset = await get_timezone_offset("MockedZone")
         self.assertEqual(offset, "1.0")
 
     @patch("pypetkitapi.utils.ZoneInfo")
     @patch("pypetkitapi.utils.datetime")
-    def test_get_timezone_offset_no_offset(self, mock_datetime, mock_zoneinfo):
+    async def test_get_timezone_offset_no_offset(self, mock_datetime, mock_zoneinfo):
         # Mock datetime and ZoneInfo
         mock_zoneinfo.return_value = "MockedZone"
         mock_now = mock_datetime.now.return_value
         mock_now.utcoffset.return_value = None
 
-        offset = get_timezone_offset("MockedZone")
+        offset = await get_timezone_offset("MockedZone")
         self.assertEqual(offset, "0.0")
 
     @patch("pypetkitapi.utils.ZoneInfo", side_effect=ZoneInfoNotFoundError)
-    def test_get_timezone_offset_invalid_zone(self, mock_zoneinfo):
+    async def test_get_timezone_offset_invalid_zone(self, mock_zoneinfo):
         offset = get_timezone_offset("InvalidZone")
         self.assertEqual(offset, "0.0")
 
     @patch("pypetkitapi.utils.ZoneInfo")
     @patch("pypetkitapi.utils.datetime")
-    def test_get_timezone_offset_attribute_error(self, mock_datetime, mock_zoneinfo):
+    async def test_get_timezone_offset_attribute_error(
+        self, mock_datetime, mock_zoneinfo
+    ):
         # Mock datetime and ZoneInfo
         mock_zoneinfo.return_value = "MockedZone"
         mock_datetime.now.side_effect = AttributeError
 
-        offset = get_timezone_offset("MockedZone")
+        offset = await get_timezone_offset("MockedZone")
         self.assertEqual(offset, "0.0")
 
 
