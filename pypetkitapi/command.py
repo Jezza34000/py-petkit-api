4c7f32631fdaa6d0397cfabd00e7412c85ef5943
Jezza34000 <57314417+Jezza34000@users.noreply.github.com>
2026-05-06 21:17:01 +0200

    :sparkles: add new feeder endpoint

--- pypetkitapi/command.py
+++ pypetkitapi/command.py
@@ -8,6 +8,7 @@ import json
 
 from pypetkitapi.const import (
     ALL_DEVICES,
+    COZY,
     D3,
     D4H,
     D4S,
@@ -59,6 +60,9 @@ class FeederCommand(StrEnum):
     REMOVE_DAILY_FEED = "remove_daily_feed"
     RESTORE_DAILY_FEED = "restore_daily_feed"
     SAVE_FEED = "save_feed"
+    SUSPEND_FEED = "suspend_feed"
+    RESTORE_FEED = "restore_feed"
+    SAVE_REPEATS = "save_repeats"
     PLAY_SOUND = "play_sound"
 
 
@@ -178,6 +182,27 @@ def get_endpoint_update_setting(device):
     return PetkitEndpoint.UPDATE_SETTING
 
 
+def get_endpoint_suspend_feed(device):
+    """Get the suspend feed endpoint for the device."""
+    if device.device_nfo.device_type in [FEEDER_MINI, FEEDER]:
+        return PetkitEndpoint.SUSPEND_FEED_OLD
+    return PetkitEndpoint.SUSPEND_FEED_NEW
+
+
+def get_endpoint_restore_feed(device):
+    """Get the restore feed endpoint for the device."""
+    if device.device_nfo.device_type in [FEEDER_MINI, FEEDER]:
+        return PetkitEndpoint.RESTORE_FEED_OLD
+    return PetkitEndpoint.RESTORE_FEED_NEW
+
+
+def get_endpoint_save_repeats(device):
+    """Get the save repeats endpoint for the device."""
+    if device.device_nfo.device_type in [FEEDER_MINI, FEEDER]:
+        return PetkitEndpoint.SAVE_REPEATS_OLD
+    return PetkitEndpoint.SAVE_REPEATS_NEW
+
+
 def validate_manual_feed_amount(
     device,
     setting: dict[str, int],
@@ -252,7 +277,7 @@ ACTIONS_MAP = {
             "kv": json.dumps(command),
             "type": list(command.keys())[0].split("_")[0],
         },
-        supported_device=[K2, K3, T3, T4, T5, T6, T7],
+        supported_device=[K2, K3, T3, T4, T5, T6, T7, COZY],
     ),
     DeviceCommand.OPEN_CAMERA: CmdData(
         endpoint=PetkitEndpoint.TEMP_OPEN_CAMERA,
@@ -329,6 +354,28 @@ ACTIONS_MAP = {
         params=lambda device, feed_daily_list: {
             "deviceId": device.id,
             "feedDailyList": json.dumps(feed_daily_list),
+        },
+        supported_device=DEVICES_FEEDER,
+    ),
+    FeederCommand.SUSPEND_FEED: CmdData(
+        endpoint=get_endpoint_suspend_feed,
+        params=lambda device: {
+            "deviceId": device.id,
+        },
+        supported_device=DEVICES_FEEDER,
+    ),
+    FeederCommand.RESTORE_FEED: CmdData(
+        endpoint=get_endpoint_restore_feed,
+        params=lambda device: {
+            "deviceId": device.id,
+        },
+        supported_device=DEVICES_FEEDER,
+    ),
+    FeederCommand.SAVE_REPEATS: CmdData(
+        endpoint=get_endpoint_save_repeats,
+        params=lambda device, setting: {
+            "deviceId": device.id,
+            **setting,
         },
         supported_device=DEVICES_FEEDER,
     ),
